cd2af5a9d52d2aac104e417b341a244fb3ae71b2
amraks85 <43678213+amraks85@users.noreply.github.com>
2018-09-30 15:52:27 +1000

    background.js
    added the advocate and examiner

--- background.js
+++ background.js
@@ -43,7 +43,9 @@ var defaultSites = {
   'The Sydney Morning Herald': 'smh.com.au',
   'The Washington Post': 'washingtonpost.com',
   'The Wall Street Journal': 'wsj.com',
-  'Wired': 'wired.com'
+  'Wired': 'wired.com',
+  'The Advocate': 'theadvocate.com.au',
+  'Examiner': 'examiner.com.au'
 };
 
 const restrictions = {
